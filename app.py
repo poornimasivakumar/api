94f3cd6e57f356195d2f89b74a229b41e644c1d5
poornimasivakumar <poornima@oodi.co.in>
2017-09-25 09:12:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,32 +50,45 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "getjoke":
+    if req.get("result").get("action")=="yahooWeatherForecast":
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = makeYqlQuery(req)
+        if yql_query is None:
+           return {}
+        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+        result = urlopen(yql_url).read()
+        data = json.loads(result)
+        res = makeWebhookResult(data)
+    elif req.get("result").get("action")=="getjoke":
+        baseurl = "http://api.icndb.com/jokes/random"
+        result = urlopen(baseurl).read()
+        data = json.loads(result)
+        res = makeWebhookResultForGetJoke(data)
+    else:
         return {}
-    baseurl = "http://api.icndb.com/jokes/random"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+
     return res
 
-
-def makeYqlQuery(req):
-    result = req.get("result")
-    joke = result.get("joke")
-	speechText = joke
+def makeWebhookResultForGetJoke(data):
+    valueString = data.get('value')
+    joke = valueString.get('joke')
+    speechText = joke
     displayText = joke
-
     return {
-		"speech": speechText,
+        "speech": speechText,
         "displayText": displayText,
         # "data": data,
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
-	} 
+    }
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
