d6e27b2c72fe164d1fa2108cbe5d070330463dc1
poornimasivakumar <poornima@oodi.co.in>
2017-09-25 16:21:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,12 +93,12 @@ def getdatafrombackend(data):
     order_no = valueString.get('order_no')
     speechText = order_no
     displayText = order_no
-	print order_no
+    print order_no
     return {
         "speech": speechText,
         "displayText": displayText,
          #data": data,
-        # "contextOut": [],
+         #"contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
 def makeYqlQuery(req):
